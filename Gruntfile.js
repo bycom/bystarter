c60571b90d0293ba3ffe2404d71cb20194453716
Sérgio Santos <me@s3rgiosan.com>
2014-06-04 23:14:40 +0100

    Removed duplicate line

--- Gruntfile.js
+++ Gruntfile.js
@@ -151,7 +151,6 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-combine-media-queries');
