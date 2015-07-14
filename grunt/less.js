2ec6a2f3cbdf1710bc53f16baa04a597f3d91b60
Marocas <bymario.silva@bycom.com.pt>
2015-07-14 16:02:14 +0100

    Sourcemap fixe.

--- grunt/less.js
+++ grunt/less.js
@@ -1,23 +1,25 @@
 'use strict';
 
 module.exports = {
-  compile: {
-    options: {
-      paths: [
-        "<%= cfg.root %><%= cfg.src.styles %>"
-      ]
+	compile: {
+		options: {
+			paths: [
+				"<%= cfg.root %><%= cfg.src.styles %>"
+			]
+		},
+		files: {
+			"<%= cfg.root %><%= cfg.dest.styles %>main.css" :
+			"<%= cfg.root %><%= cfg.src.styles %>main.less"
+		}
+	},
+	sourceMap: {
+		options : {
+      optimization : 2,
+      sourceMap : true,
+      sourceMapFileInline : true,
+      dumpLineNumbers : 'false'
     },
-    files: {
-      "<%= cfg.root %><%= cfg.dest.styles %>main.css" :
-      "<%= cfg.root %><%= cfg.src.styles %>main.less"
-    }
-  },
-  sourceMap: {
-    options: {
-      "optimization": 2,
-      "sourceMap": true
-    },
-    src: "<%= cfg.root %><%= cfg.src.styles %>main.less",
-    dest: "<%= cfg.root %><%= cfg.dest.styles %>main.css"
-  }
+		src: "<%= cfg.root %><%= cfg.src.styles %>main.less",
+		dest: "<%= cfg.root %><%= cfg.dest.styles %>main.css"
+	}
 };
