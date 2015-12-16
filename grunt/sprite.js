1533b8cce6cbf98a54cf1f307da747dfab674e83
marocas <marioposilva@gmail.com>
2015-12-16 11:03:07 +0000

    Update spritesmith.js

--- grunt/sprite.js
+++ grunt/sprite.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   icons: {
-    // src: "<%= cfg.root %><%= cfg.src.images %>icons/*.png",
-    // dest: "<%= cfg.root %><%= cfg.dest.images %>icons.png",
-    // destCSS: "<%= cfg.root %><%= cfg.src.styles %>utilities/icons.less",
-    // algorithm: "binary-tree",
-    // engine: "phantomjs"
-    src: "<%= cfg.root %><%= cfg.src.images %>icons/*.png",
-    dest: "<%= cfg.root %><%= cfg.dest.images %>icons.png",
-    destCss: "<%= cfg.root %><%= cfg.src.styles %>utilities/icons.less"
+    src: '<%= cfg.root %><%= cfg.src.images %>icons/*.png',
+		dest: '<%= cfg.root %><%= cfg.dest.images %>icons.png',
+		destCss: '<%= cfg.root %><%= cfg.src.styles %>utilities/icons.less',
+		engine: 'pixelsmith',
+		algorithm: 'binary-tree',
+		algorithmOpts: {
+			sort: false
+		}
   }
 };
