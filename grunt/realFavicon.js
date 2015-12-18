fa37bd9d0040412526408c644131517cec9117d0
marocas <marioposilva@gmail.com>
2015-12-18 14:16:06 +0000

    Create realFavicon.js

---
+++ grunt/realFavicon.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+	favicons: {
+		src: '<%= cfg.root %><%= cfg.src.images %>favicon.png',
+		dest: '<%= cfg.root %><%= cfg.dest.html %>',
+		options: {
+			iconsPath: '/',
+			html: [ '<%= cfg.root %><%= cfg.src.utils %>globals/head.html' ],
+			design: {
+				ios: {
+					pictureAspect: 'noChange'
+				},
+				desktopBrowser: {},
+				windows: {
+					pictureAspect: 'noChange',
+					backgroundColor: '#da532c',
+					onConflict: 'override'
+				},
+				androidChrome: {
+					pictureAspect: 'noChange',
+					themeColor: '#ffffff',
+					manifest: {
+						name: 'almaria',
+						display: 'browser',
+						orientation: 'notSet',
+						onConflict: 'override'
+					}
+				},
+				safariPinnedTab: {
+					pictureAspect: 'silhouette',
+					themeColor: '#5bbad5'
+				}
+			},
+			settings: {
+				scalingAlgorithm: 'Mitchell',
+				errorOnImageTooSmall: false
+			}
+		}
+	}
+};
